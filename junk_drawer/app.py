96df6fd1cb0012765fd5f5a1ecfcba34a7290b55
William Jackson <565174+williamjacksn@users.noreply.github.com>
2025-06-05 08:52:30 -0500

    Ruff format

--- junk_drawer/app.py
+++ junk_drawer/app.py
@@ -12,38 +12,42 @@ import werkzeug.utils
 settings = junk_drawer.settings.Settings()
 
 app = flask.Flask(__name__)
-app.wsgi_app = werkzeug.middleware.proxy_fix.ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_port=1)
+app.wsgi_app = werkzeug.middleware.proxy_fix.ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_port=1
+)
 
 app.secret_key = settings.secret_key
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if flask.session.get('signed-in'):
-        if flask.request.method == 'GET':
-            return flask.render_template('upload-file.html')
-        file = flask.request.files.get('file')
+    if flask.session.get("signed-in"):
+        if flask.request.method == "GET":
+            return flask.render_template("upload-file.html")
+        file = flask.request.files.get("file")
         filename = werkzeug.utils.secure_filename(file.filename)
         ext = pathlib.Path(filename).suffix
         u = str(uuid.uuid4())[:8]
-        filename = f'{datetime.datetime.utcnow():%Y-%m-%d-%H%M%S}-{u}{ext}'
+        filename = f"{datetime.datetime.utcnow():%Y-%m-%d-%H%M%S}-{u}{ext}"
         file.save(str(settings.file_upload_dir / filename))
-        return flask.redirect(flask.url_for('index'))
-    return flask.render_template('sign-in.html')
+        return flask.redirect(flask.url_for("index"))
+    return flask.render_template("sign-in.html")
 
 
-@app.route('/sign-in', methods=['POST'])
+@app.route("/sign-in", methods=["POST"])
 def sign_in():
-    password = flask.request.values.get('password')
+    password = flask.request.values.get("password")
     if password == settings.password:
-        flask.session['signed-in'] = True
-    return flask.redirect(flask.url_for('index'))
+        flask.session["signed-in"] = True
+    return flask.redirect(flask.url_for("index"))
 
 
 def main():
-    logging.basicConfig(format=settings.log_format, level=logging.DEBUG, stream=sys.stdout)
-    app.logger.debug(f'junk-drawer {settings.version}')
-    if settings.log_level != 'DEBUG':
-        app.logger.debug(f'Changing log level to {settings.log_level}')
+    logging.basicConfig(
+        format=settings.log_format, level=logging.DEBUG, stream=sys.stdout
+    )
+    app.logger.debug(f"junk-drawer {settings.version}")
+    if settings.log_level != "DEBUG":
+        app.logger.debug(f"Changing log level to {settings.log_level}")
     logging.getLogger().setLevel(settings.log_level)
     waitress.serve(app, ident=None)
